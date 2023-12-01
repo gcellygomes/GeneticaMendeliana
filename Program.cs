d5b06bb6f537e78f683bbdd8acb2737379e00bdb
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-12-01 15:22:20 -0300

    Projeto Mendeliana

--- Program.cs
+++ Program.cs
@@ -1,86 +1,84 @@
 ﻿//Projeto GicellyGomes ***GeneticaMediliana***
+[quarta-feira 19:40] GICELLY APARECIDA GOMES
 using System;
-
-namespace GeneticaMendeliana
+ 
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("--- Genética Mendeliana ---");
+        Console.Write("Alelos do indivíduo 1 (AA, Aa ou aa): ");
+        string alelosIndividuo1 = Console.ReadLine()!.ToUpper();
+ 
+        Console.Write("Alelos do indivíduo 2 (AA, Aa ou aa): ");
+        string alelosIndividuo2 = Console.ReadLine()!.ToUpper();
+ 
+        Console.Write("Tipo de dominância (C/I): ");
+        char tipoDominancia = Console.ReadLine()!.ToUpper()[0];
+ 
+        CalculaProbabilidades(alelosIndividuo1, alelosIndividuo2, tipoDominancia);
+ 
+        Console.ReadKey();
+    }
+ 
+    static void CalculaProbabilidades(string alelosIndividuo1, string alelosIndividuo2, char tipoDominancia)
+    {
+        double probabilidadeAA = 0, probabilidadeAa = 0, probabilidadeaa = 0;
+ 
+        // Verifica os possíveis genótipos dos descendentes e calcula as probabilidades
+        if (tipoDominancia == 'C')
         {
-            Console.WriteLine("--- Genética Mendeliana ---");
-            Console.Write("Alelos do indivíduo 1 (AA, Aa ou aa): ");
-            string alelosIndividuo1 = Console.ReadLine().ToUpper();
-            
-            Console.Write("Alelos do indivíduo 2 (AA, Aa ou aa): ");
-            string alelosIndividuo2 = Console.ReadLine().ToUpper();
-            
-            Console.Write("Tipo de dominância (C/I): ");
-            char tipoDominancia = Console.ReadLine().ToUpper()[0];
-
-            CalculaProbabilidades(alelosIndividuo1, alelosIndividuo2, tipoDominancia);
-
-            Console.ReadKey();
+            if ((alelosIndividuo1 == "AA" || alelosIndividuo1 == "Aa") && (alelosIndividuo2 == "AA" || alelosIndividuo2 == "Aa"))
+            {
+                probabilidadeAA = 1;
+                probabilidadeAa = 0;
+                probabilidadeaa = 0;
+            }
+            else if ((alelosIndividuo1 == "aa") && (alelosIndividuo2 == "aa"))
+            {
+                probabilidadeAA = 0;
+                probabilidadeAa = 0;
+                probabilidadeaa = 1;
+            }
+            else
+            {
+                probabilidadeAA = 0;
+                probabilidadeAa = 0.5;
+                probabilidadeaa = 0.5;
+            }
         }
-
-        static void CalculaProbabilidades(string alelosIndividuo1, string alelosIndividuo2, char tipoDominancia)
+        else if (tipoDominancia == 'I')
         {
-            double probabilidadeAA = 0, probabilidadeAa = 0, probabilidadeaa = 0;
-
-            // Verifica os possíveis genótipos dos descendentes e calcula as probabilidades
-            if (tipoDominancia == 'C')
+            if ((alelosIndividuo1 == "AA") && (alelosIndividuo2 == "AA"))
             {
-                if ((alelosIndividuo1 == "AA" || alelosIndividuo1 == "Aa") && (alelosIndividuo2 == "AA" || alelosIndividuo2 == "Aa"))
-                {
-                    probabilidadeAA = 1;
-                    probabilidadeAa = 0;
-                    probabilidadeaa = 0;
-                }
-                else if ((alelosIndividuo1 == "aa") && (alelosIndividuo2 == "aa"))
-                {
-                    probabilidadeAA = 0;
-                    probabilidadeAa = 0;
-                    probabilidadeaa = 1;
-                }
-                else
-                {
-                    probabilidadeAA = 0;
-                    probabilidadeAa = 0.5;
-                    probabilidadeaa = 0.5;
-                }
+                probabilidadeAA = 1;
+                probabilidadeAa = 0;
+                probabilidadeaa = 0;
             }
-            else if (tipoDominancia == 'I')
+            else if ((alelosIndividuo1 == "aa") && (alelosIndividuo2 == "aa"))
             {
-                if ((alelosIndividuo1 == "AA") && (alelosIndividuo2 == "AA"))
-                {
-                    probabilidadeAA = 1;
-                    probabilidadeAa = 0;
-                    probabilidadeaa = 0;
-                }
-                else if ((alelosIndividuo1 == "aa") && (alelosIndividuo2 == "aa"))
-                {
-                    probabilidadeAA = 0;
-                    probabilidadeAa = 0;
-                    probabilidadeaa = 1;
-                }
-                else if ((alelosIndividuo1 == "Aa" && alelosIndividuo2 == "AA") || (alelosIndividuo1 == "AA" && alelosIndividuo2 == "Aa"))
-                {
-                    probabilidadeAA = 0.25;
-                    probabilidadeAa = 0.5;
-                    probabilidadeaa = 0.25;
-                }
-                else
-                {
-                    probabilidadeAA = 0;
-                    probabilidadeAa = 0.5;
-                    probabilidadeaa = 0.5;
-                }
+                probabilidadeAA = 0;
+                probabilidadeAa = 0;
+                probabilidadeaa = 1;
             }
-
-            // Exibe as probabilidades e as características associadas a elas
-            Console.WriteLine("--- Genética Mendeliana ---");
-            Console.WriteLine($"AA: {(probabilidadeAA * 100):0}% - {(tipoDominancia == 'C' ? "verde" : "magenta")}");
-            Console.WriteLine($"Aa: {(probabilidadeAa * 100):0}% - {(tipoDominancia == 'C' ? "verde" : "magenta")}");
-            Console.WriteLine($"aa: {(probabilidadeaa * 100):0}% - {(tipoDominancia == 'C' ? "verde" : "magenta")}");
+            else if ((alelosIndividuo1 == "Aa" && alelosIndividuo2 == "AA") || (alelosIndividuo1 == "AA" && alelosIndividuo2 == "Aa"))
+            {
+                probabilidadeAA = 0.25;
+                probabilidadeAa = 0.5;
+                probabilidadeaa = 0.25;
+            }
+            else
+            {
+                probabilidadeAA = 0;
+                probabilidadeAa = 0.5;
+                probabilidadeaa = 0.5;
+            }
         }
+ 
+        // Exibe as probabilidades e as características associadas a elas
+        Console.WriteLine("--- Genética Mendeliana ---");
+        Console.WriteLine($"AA: {(probabilidadeAA * 100):0}% - {(tipoDominancia == 'C' ? "verde" : "magenta")}");
+        Console.WriteLine($"Aa: {(probabilidadeAa * 100):0}% - {(tipoDominancia == 'C' ? "verde" : "magenta")}");
+        Console.WriteLine($"aa: {(probabilidadeaa * 100):0}% - {(tipoDominancia == 'C' ? "verde" : "magenta")}");
     }
 }
